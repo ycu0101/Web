7a600bd9d9415105db1e0c634e20edcf18aba963
Administrator <Administrator@1301002-NB>
2019-12-30 18:05:56 +0800

    Test Interator and Normal Query

---
+++ WebPractice/DesignPatternPractice/Interator/InteratorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternPractice.Interator
+{
+    public class InteratorTest : System.Collections.IEnumerable
+    {
+        private IEnumerable<OrderList> _list;
+
+        /// <summary>
+        /// 在建構此類別時必須放入集合
+        /// </summary>
+        /// <param name="_list">IEnumable<T> 類型的集合</param>
+        public InteratorTest(IEnumerable<OrderList> _temp)
+        {
+            _list = _temp;
+        }
+
+        /// <summary>
+        /// Enumerator
+        /// </summary>
+        /// <returns>return: IEnumerator</returns>
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in _list)
+            {
+                yield return item;
+            }
+        }
+    }
+
+
+    public class OrderList
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
--- WebPractice/DesignPatternPractice/Program.cs
+++ WebPractice/DesignPatternPractice/Program.cs
@@ -3,13 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesignPatternPractice._DB;
+using DesignPatternPractice.Interator;
 
 namespace DesignPatternPractice
 {
     class Program
     {
+        private static NW_Entities _nw = new NW_Entities();
+        private static System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+
         static void Main(string[] args)
         {
+            double t1;
+
+            #region Interator vs Normal Query
+            //timer.Reset();
+            ////Normal Query
+            //var list_linq = _nw.Orders.Select(_o => new OrderList { OrderID = _o.OrderID, CustomerID = _o.CustomerID });
+            //timer.Start();
+            //foreach (var item in list_linq)
+            //{
+            //    Console.WriteLine("OrderID : {0}, Customer : {1}\t||\t", item.OrderID, item.CustomerID);
+            //}
+            //timer.Stop();
+            //t1 = timer.Elapsed.TotalMilliseconds;
+
+            //Console.WriteLine("\n========================================\n");
+
+            timer.Reset();
+            //Use Interator
+            IEnumerable<OrderList> list_linq2 = _nw.Orders.Select(_o => new OrderList { OrderID = _o.OrderID, CustomerID = _o.CustomerID });
+            timer.Start();
+            //IEnumerator<Order> list_interator = list_linq2.GetEnumerator();
+            foreach (OrderList item in new InteratorTest(list_linq2))
+            {
+                Console.WriteLine("OrderID : {0}, Customer : {1}\t||\t", item.OrderID, item.CustomerID);
+            }
+            timer.Stop();
+            t1 = timer.Elapsed.TotalMilliseconds;
+            #endregion
+
+
+
+            Console.WriteLine("Time1 : {0}", t1);
+            Console.Write("\nOver...");
+            Console.ReadKey();
         }
     }
+
+
 }
