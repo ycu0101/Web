45cd8e4ba27360b838eb6e113b506e9af738cee2
James <chunju1123@gmail.com>
2020-01-16 16:00:29 +0800

    加入 MessageController, MessageHome index 加入 Content_Cus 放入自製的 css 加入 Content_Cus 到 _Layout.cshtml 測試 RouteConfig Routing 功能(沒測完)

--- WebPractice/MessageBoard/App_Start/RouteConfig.cs
+++ WebPractice/MessageBoard/App_Start/RouteConfig.cs
@@ -14,10 +14,21 @@ namespace MessageBoard
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
+                name: "Home",
+                url: "{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+            // TODO: 新增多個 Routing
+            //routes.MapRoute(name: "Message", url: "{action}/{id}", defaults: new { controller = "Message", action = "MessageHome", id = UrlParameter.Optional });
+
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ WebPractice/MessageBoard/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MessageBoard.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        public ActionResult MessageHome()
+        {
+            return View();
+        }
+    }
+}
